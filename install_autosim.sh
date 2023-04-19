677c0742c1851f1a1a4595bf60b03a2f9a7a7099
B. Ionut <61597084+bionut15@users.noreply.github.com>
2023-04-19 15:34:56 +0300

    Update install_autosim.sh

--- install_autosim.sh
+++ install_autosim.sh
@@ -10,7 +10,7 @@ WINE_PREFIX=~/autosim WINEARCH=win32 wine regedit config.reg
 wget $(cat link.txt) -O ~/autosim/as.zip
 unzip ~/autosim/as.zip -d ~/autosim/as
 WINE_PREFIX=~/autosim WINEARCH=win32 wine ~/autosim/as/as200v410na.exe
-p ~/autosim/drive_c/Program\ Files/SMC/AUTOSIM-200V4/dzip.dll ~/autosim/drive_c/Program\ Files/SMC/AUTOSIM-200V4/DZIP.dll
+cp ~/autosim/drive_c/Program\ Files/SMC/AUTOSIM-200V4/dzip.dll ~/autosim/drive_c/Program\ Files/SMC/AUTOSIM-200V4/DZIP.dll
 
 cp ~/autosim/drive_c/Program\ Files/SMC/AUTOSIM-200V4/dunzip.dll ~/autosim/drive_c/Program\ Files/SMC/AUTOSIM-200V4/DUNZIP.dll
 ./launch.sh
